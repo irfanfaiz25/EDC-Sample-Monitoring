1920307495f2f1e67a68c3d07a10d4376a6d9d45
irfanfaiz25 <ir.faiz2000@gmail.com>
2023-09-04 05:42:36 +0700

    memperbaiki tampilan index dan juga merapihkan coding halaman index, dan memperbaiki user setting admin, dan menambahkan hak akses user

--- header.php
+++ header.php
@@ -48,21 +48,16 @@ $curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/style-trackk.css">
     <link rel="stylesheet" href="css/card-setting.css">
-    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <!-- data table -->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-    <link rel="stylesheet" type="text/css" href="https://datatables.net/media/css/site-examples.css">
-
-
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/dataTables.bootstrap5.min.css" />
 
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
-    <!-- <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script> -->
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
--- setting-admin.php
+++ setting-admin.php
@@ -21,18 +21,23 @@ include "fungsi/fungsi-setting.php";
             <div class="padding">
                 <div class="row container d-flex justify-content-center">
                     <?php
-                    foreach ($admin_set as $row) :
-                    ?>
+                    foreach ($admin_set as $row):
+                        ?>
                         <div class="col-xl-6 col-md-12">
                             <div class="card user-card-full">
                                 <div class="row m-l-0 m-r-0">
                                     <div class="col-sm-4 bg-c-lite-green user-profile">
                                         <div class="card-block text-center text-white">
                                             <div class="m-b-25">
-                                                <img src="img/user-img/<?= $row["foto"]; ?>" width="80" class="img-radius" alt="User-Profile-Image">
+                                                <img src="img/user-img/<?= $row["foto"]; ?>" width="80" class="img-radius"
+                                                    alt="User-Profile-Image">
                                             </div>
-                                            <h6 class="f-w-600"><?= $row["nama"]; ?></h6>
-                                            <p><?= $row["level_user"]; ?></p>
+                                            <h6 class="f-w-600">
+                                                <?= $row["nama"]; ?>
+                                            </h6>
+                                            <p>
+                                                <?= $row["level_user"]; ?>
+                                            </p>
                                             <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
                                         </div>
                                     </div>
@@ -45,14 +50,21 @@ include "fungsi/fungsi-setting.php";
                                             <div class="row">
                                                 <div class="col-sm-6 mt-2">
                                                     <p class="m-b-10 f-w-600">Username</p>
-                                                    <h6 class="text-muted f-w-400"><?= $row["username"]; ?></h6>
+                                                    <h6 class="text-muted f-w-400">
+                                                        <?= $row["username"]; ?>
+                                                    </h6>
                                                 </div>
                                                 <div class="col-sm-6 mt-2">
                                                     <p class="m-b-10 f-w-600">Position</p>
-                                                    <h6 class="text-muted f-w-400"><?= $row["level_user"]; ?></h6>
+                                                    <h6 class="text-muted f-w-400">
+                                                        <?= $row["level_user"]; ?>
+                                                    </h6>
                                                 </div>
                                                 <div class="col-md-12">
-                                                    <button class="btn btn-primary btn-sm float-end me-3 mt-xl-5" data-toggle="modal" data-target="#editUser<?= $row["id"]; ?>"><strong>EDIT </strong><i class="fa fa-gears"></i></button>
+                                                    <button class="btn btn-primary btn-sm float-end me-3 mt-xl-5"
+                                                        data-toggle="modal"
+                                                        data-target="#editUser<?= $row["id"]; ?>"><strong>EDIT </strong><i
+                                                            class="fa fa-gears"></i></button>
                                                 </div>
                                             </div>
                                         </div>
@@ -67,14 +79,24 @@ include "fungsi/fungsi-setting.php";
                                 <div class="modal-content">
                                     <div class="modal-header">
                                         <h4 class="text-black">
-                                            Edit password
+                                            Edit User
                                         </h4>
-                                        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                                        <button type="button" class="btn-close" data-dismiss="modal"
+                                            aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body text-black">
                                         <div class="row">
                                             <div class="col-md-3 border-right">
-                                                <div class="d-flex flex-column align-items-center text-center p-0 py-3 pt-0"><img class="img-radius mt-5" id="img" width="150px" src="img/user-img/<?= $row["foto"]; ?>"><span class="text-black pt-2"><?= $row["nama"]; ?></span><span class="text-black-50"><?= $row["level_user"]; ?></span><span></span></div>
+                                                <div
+                                                    class="d-flex flex-column align-items-center text-center p-0 py-3 pt-0">
+                                                    <img class="img-radius mt-5" id="img<?= $row["id"]; ?>" width="150px"
+                                                        src="img/user-img/<?= $row["foto"]; ?>"><span
+                                                        class="text-black pt-2">
+                                                        <?= $row["nama"]; ?>
+                                                    </span><span class="text-black-50">
+                                                        <?= $row["level_user"]; ?>
+                                                    </span><span></span>
+                                                </div>
                                             </div>
                                             <div class="col-md-9 border-right pe-5">
                                                 <div class="p-3 py-5">
@@ -84,25 +106,33 @@ include "fungsi/fungsi-setting.php";
                                                         </div>
                                                         <div class="row mt-3 text-black">
                                                             <div class="col-md-3">
-                                                                <input type="file" class="custom-file-input pt-0 pb-2" name="foto" id="upload">
+                                                                <input type="file" class="custom-file-input pt-0 pb-2"
+                                                                    name="foto" id="foto"
+                                                                    onchange="document.getElementById('img<?= $row['id']; ?>').src = window.URL.createObjectURL(this.files[0])">
                                                             </div>
                                                             <div class="col-md-2 mt-2 me-4">
-                                                                <label for="upload"><i class="fa fa-images fa-2xl"></i></label>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <button type="button" class="btn btn-warning float-end" data-toggle="modal" data-target="#editPass">Change password <i class="fa fa-key"></i></button>
+                                                                <label for="upload"><i
+                                                                        class="fa fa-images fa-2xl"></i></label>
                                                             </div>
                                                             <input type="text" name="id" value="<?= $row["id"]; ?>" hidden>
-                                                            <input type="text" name="foto_old" value="<?= $row["foto"]; ?>" hidden>
+                                                            <input type="text" name="foto_old" value="<?= $row["foto"]; ?>"
+                                                                hidden>
                                                             <div class="col-md-12"><label class="labels">Username</label>
-                                                                <input type="text" class="form-control" name="username" placeholder="" value="<?= $row["username"]; ?>" disabled>
+                                                                <input type="text" class="form-control" name="username"
+                                                                    placeholder="" value="<?= $row["username"]; ?>"
+                                                                    disabled>
                                                             </div>
                                                             <div class="col-md-12"><label class="labels">Name</label>
-                                                                <input type="text" class="form-control" name="nama" placeholder="" value="<?= $row["nama"]; ?>">
+                                                                <input type="text" class="form-control" name="nama"
+                                                                    placeholder="" value="<?= $row["nama"]; ?>">
                                                             </div>
-                                                            <div class="col-md-12"><label class="labels pt-2">Position</label>
-                                                                <select class="form-select" aria-label=".form-select-sm example" name="level">
-                                                                    <option selected><?= $row["level_user"]; ?></option>
+                                                            <div class="col-md-12"><label
+                                                                    class="labels pt-2">Position</label>
+                                                                <select class="form-select"
+                                                                    aria-label=".form-select-sm example" name="level">
+                                                                    <option selected>
+                                                                        <?= $row["level_user"]; ?>
+                                                                    </option>
                                                                     <option value="marketing">Marketing</option>
                                                                     <option value="lab">Lab</option>
                                                                     <option value="testing">Testing</option>
@@ -115,7 +145,8 @@ include "fungsi/fungsi-setting.php";
                                     </div>
 
                                     <div class="modal-footer">
-                                        <button type="submit" class="btn btn-success" name="btn-update-admin"><i class="fa fa-floppy-disk"></i>
+                                        <button type="submit" class="btn btn-success" name="btn-update-admin"><i
+                                                class="fa fa-floppy-disk"></i>
                                             SAVE</button>
                                         </form>
                                     </div>
@@ -124,7 +155,7 @@ include "fungsi/fungsi-setting.php";
                         </div>
                         <!-- end modal detail -->
                         </form>
-                    <?php
+                        <?php
                     endforeach;
                     ?>
 
